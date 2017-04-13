50669352a133ace2e11660226d553b94566f79ce
michaelkkim <keehankim@gmail.com>
2017-04-13 10:03:57 -0400

    updates

--- code/helpful_functions.py
+++ code/helpful_functions.py
@@ -19,3 +19,20 @@ def case_info(op_id):
     print cl_data['case_name']
     print 'date_filed: %s' % cl_data['date_filed']
     print opinion_url
+
+def case_info2(op_id):
+    """
+    Given the case id returns a link to the opinion file on court listener
+    """
+    # API url
+    op_api = 'https://www.courtlistener.com/api/rest/v3/opinions/%s/?format=json'\
+              % op_id
+
+    # get data from opinion and cluster api
+    op_data = url_to_dict(op_api)
+    cl_data = url_to_dict(op_data['cluster'])
+
+    # url to opinion text
+    opinion_url = 'https://www.courtlistener.com' + op_data['absolute_url']
+
+    return cl_data['case_name'], cl_data['date_filed'], opinion_url
